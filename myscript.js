d050d31e4e0712901d3eef384b15d4914b872194
adri0011 <111551085+adri0011@users.noreply.github.com>
2022-10-17 20:34:25 +1100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,19 @@
+var vg_1 = "mapdrinks.vl.json";
+vegaEmbed("#map1", vg_1). then (function(result) {
+// Access the Vega view instance (https://vega.github.io.vega/docs/api/view/) as result.view 
+}).catch(console.error);  
+
+var vg_1 = "top20served.vl.json";
+vegaEmbed("#map2", vg_1). then (function(result) {
+// Access the Vega view instance (https://vega.github.io.vega/docs/api/view/) as result.view 
+}).catch(console.error);  
+
+var vg_1 = "selectionwithtooltip.vl.json";
+vegaEmbed("#map3", vg_1). then (function(result) {
+// Access the Vega view instance (https://vega.github.io.vega/docs/api/view/) as result.view 
+}).catch(console.error); 
+
+var vg_1 = "boxplotdistribution.vl.json";
+vegaEmbed("#map4", vg_1). then (function(result) {
+// Access the Vega view instance (https://vega.github.io.vega/docs/api/view/) as result.view 
+}).catch(console.error); 
